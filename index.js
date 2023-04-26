a2176739fb3a126dc605edc48a8f67daaeb51988
EjembiEmmanuel <ejembioche50@gmail.com>
2023-04-26 10:44:39 +0100

    Added feature for toggling book read status

--- index.js
+++ index.js
@@ -1,87 +1,135 @@
-let myLibrary = []
+let myLibrary = [];
 
-function Book(title, author, pages, readStatus) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.readStatus = readStatus
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
 }
+
+Book.prototype.readStatus = function (readStatus) {
+  this.readStatus = readStatus;
+};
 
 function addBook(title, author, pages, status) {
-    let newBook = new Book(title, author, pages, status)
+  let newBook = new Book(title, author, pages);
 
-    myLibrary.push(newBook)
+  newBook.readStatus(status);
+
+  myLibrary.push(newBook);
 }
 
-
 function displayBooks() {
-    const table = document.querySelector('.table')
+  const table = document.querySelector(".table");
 
-    table.innerHTML = `
+  table.innerHTML = `
         <tr>
-            <th>&nbsp;</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Pages</th>
-            <th>Status</th>
-            <th>&nbsp;</th>
+            <th style="width: 5%">&nbsp;</th>
+            <th style="width: 30%">Title</th>
+            <th style="width: 15%">Author</th>
+            <th style="width: 15%">Pages</th>
+            <th style="width: 10%">Status</th>
+            <th style="width: 25%" colspan="2">Change Status/Delete</th>
         </tr>
-    `
+    `;
 
-    for (let i = 0; i < myLibrary.length; i++) {
-        const book = myLibrary[i]
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
 
-        table.innerHTML += `
-            <tr>
-                <td>${i + 1}</td>
-                <td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.pages}</td>
-                <td>${book.readStatus}</td>
-                <td><button id="deleteBtn${i}" data-key="${i}" onclick="deleteBook(this.dataset.key)"><img src="assets/icons/delete.svg" alt=""></button></td>
-            </tr>   
-        `
+    if (book.readStatus === "Read") {
+      table.innerHTML += `
+                <tr>
+                    <td>${i + 1}</td>
+                    <td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.pages}</td>
+                    <td>
+                        ${book.readStatus}
+                    </td>
+                    <td>
+                        Mark as read: 
+                        <label class="switch">
+                            <input id="toggleBtn${i}" type="checkbox" data-key="${i}" onclick="toggleStatus(this.id)" checked>
+                            <span class="slider round"></span>
+                        </label>
+                    </td>
+                    <td><button id="deleteBtn${i}" data-key="${i}" onclick="deleteBook(this.dataset.key)"><img src="assets/icons/delete.svg" alt=""></button></td>
+                </tr>   
+            `;
+    } else {
+      table.innerHTML += `
+                <tr>
+                    <td>${i + 1}</td>
+                    <td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.pages}</td>
+                    <td>
+                        ${book.readStatus}
+                    </td>
+                    <td>
+                        Mark as read: 
+                        <label class="switch">
+                            <input id="toggleBtn${i}" type="checkbox" data-key="${i}" onclick="toggleStatus(this.id)">
+                            <span class="slider round"></span>
+                        </label>
+                    </td>
+                    <td><button id="deleteBtn${i}" data-key="${i}" onclick="deleteBook(this.dataset.key)"><img src="assets/icons/delete.svg" alt=""></button></td>
+                </tr>   
+            `;
     }
+  }
+}
+
+function toggleStatus(id) {
+  let toggleBtn = document.getElementById(id);
+  let index = Number(toggleBtn.dataset.key);
+
+  if (toggleBtn.checked) {
+    myLibrary[index].readStatus = "Read";
+  } else {
+    myLibrary[index].readStatus = "Unread";
+  }
+
+  displayBooks();
 }
 
 function deleteBook(key) {
-    let index = Number(key)
+  let index = Number(key);
 
-    myLibrary.splice(index, 1)
+  myLibrary.splice(index, 1);
 
-    displayBooks()
+  displayBooks();
 }
 
 let modal = document.getElementById("bookModal");
 let addBtn = document.getElementById("addBtn");
 let span = document.getElementsByClassName("close")[0];
 
-addBtn.onclick = function() {
+addBtn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
-let bookForm = document.getElementById("bookForm")
+let bookForm = document.getElementById("bookForm");
 
 bookForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-  
-    let title = document.getElementById("title");
-    let author = document.getElementById("author");
-    let pages = document.getElementById("pages");
-    let status = document.getElementById("status");
- 
-    addBook(title.value, author.value, pages.value, status.value)
-    displayBooks()
+  e.preventDefault();
 
-    modal.style.display = "none";
+  let title = document.getElementById("title");
+  let author = document.getElementById("author");
+  let pages = document.getElementById("pages");
+  let status = document.getElementById("status");
+
+  addBook(title.value, author.value, pages.value, status.value);
+  displayBooks();
+
+  modal.style.display = "none";
 });
